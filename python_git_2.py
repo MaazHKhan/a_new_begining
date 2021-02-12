b1a88c70aece7cd5445584a6eca612026abea3c7
Maaz <maazkhan14@gmail.com>
2021-02-12 15:06:53 +0200

    added new py file _2

---
+++ python_git_2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# This code it to test git and python
+
+
+print("Hello Git\n")
+print("I have indeed been uploaded sir")
+
+
+# some variable tests
+
+x = 5
+y = 6
+
+z = x*y
+w = x+y
+
+print("z is" ,z, "\n", "w is ",w) 
+
+
+print("This is to test if a new py file was added to my repo.")
