aaa03d48396481082afd42ddf62351243fee638b
Maaz <maazkhan14@gmail.com>
2021-02-12 14:52:42 +0200

    First Commit

---
+++ python_git_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# This code it to test git and python
+
+
+print("Hello Git\n")
+print("I have indeed been uploaded sir")
+
+
+# some variable tests
+
+x = 5
+y = 6
+
+z = x*y
+w = x+y
+
+print("z is" ,z, "\n", "w is ",w) 
